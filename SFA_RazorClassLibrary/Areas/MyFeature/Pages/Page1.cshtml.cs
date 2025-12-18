3a3ca37623c444fab5e570cb9dbc1015498ec3f2
Mark Phillipson <MPhillipson0@Gmail.com>
2025-12-18 08:44:34 +0000

    feat: add Razor class library project for MAUI integration 📱

---
+++ SFA_RazorClassLibrary/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SFA_RazorClassLibrary.MyFeature.Pages;
+
+public class Page1Model : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
